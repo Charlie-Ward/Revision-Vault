6622c767636f4941b3153ae94a5c847a017d3ce5
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-11-13 11:10:26 +0000

    Initial Commit

---
+++ Revision Vault/Revision_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Revision_VaultApp.swift
+//  Revision Vault
+//
+//  Created by Charlie Ward on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Revision_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
