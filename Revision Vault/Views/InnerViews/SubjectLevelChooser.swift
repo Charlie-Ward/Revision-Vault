d98ad958b5de13fa10bb3629a26e998a3ad79a1e
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-11-17 12:49:10 +0000

    Create SubjectLevelChooser.swift

---
+++ Revision Vault/Views/InnerViews/SubjectLevelChooser.swift
@@ -0,0 +1,18 @@
+//
+//  SubjectLevelChooser.swift
+//  Revision Vault
+//
+//  Created by Charlie Ward on 17/11/2023.
+//
+
+import SwiftUI
+
+struct SubjectLevelChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubjectLevelChooser()
+}
