3983beae90a21ac48c507379ffe66cc2caaf57d4
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-11-17 06:43:35 +0000

    Made home page

--- Revision Vault/Views/MainViews/HomeView.swift
+++ Revision Vault/Views/MainViews/HomeView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject var modelData: ModelData
     var body: some View {
-        Text("Home")
+        NavigationStack{
+            List{
+                PageView(pages: modelData.features.map {
+                    FeatureCard(subject: $0)})
+                .aspectRatio(3 / 2, contentMode: .fit)
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)}
+            }
+            .listStyle(.inset)
+            .listRowInsets(EdgeInsets())
+            .navigationTitle("Home")
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View{
         HomeView()
+            .environmentObject(ModelData())
     }
 }
